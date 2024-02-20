a8f3b03886f6fad3ec1f4e7a28fefef2bf75d6d1
Karam1732 <=>
2024-02-20 19:15:14 +0100

    esta es mi parte de lo de las clases

---
+++ EL1_Karam_Manuel/src/es/uah/matcomp/mp/e1/ejerciciosclases/Circle.java
@@ -0,0 +1,46 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+public class Circle {
+    private double radius;
+    private String color;
+
+
+    public  Circle(){
+        radius = 1.0;
+        color = "red";
+    }
+
+    public Circle (double radius) {
+        this.radius = radius;
+    }
+    public Circle (double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius () {
+        return radius;
+    }
+
+    public void setRadius (double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+
+    public double getCircumference(){
+        return 2*radius*Math.PI;
+    }
+
+    public String toString(){
+        return "Circle[radius="+ radius +"]";
+
+    }
+
+
+
+
+}
